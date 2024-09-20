f572df222e7944097b3efea8c1f646a9bd0067fc
stepheniew <161106178+stepheniew@users.noreply.github.com>
2024-09-20 11:01:44 +0700

    update

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const http = require('http');
 
 // buat server 
 const server = http.createServer((req,res) => {
-    res.writeHead(200,{'Content-Type' : 'text/html'});
-    res.write('Selamat pagi');
+    //res.writeHead(200,{'Content-Type' : 'text/html'});
+    //res.write('Selamat pagi');
+
+    res.writeHead(200,{'Content-Type' : 'application/json'});
+    res.write(JSON.stringify({
+        'message' : 'selamat pagii',
+        'status'  : 'success' 
+    }));
     res.end();
 });
 
